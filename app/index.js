36245eab7d44285bbc17ad26315740842f1ffccb
10778820 <dieromram@alu.edu.gva.es>
2026-01-28 17:24:01 +0100

    Añadiendo el cerebro del despliegue

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo desde Ansible y GitHub Actions\n');
+  res.end('HOLA HOLITA\n');
 });
 server.listen(3000, '0.0.0.0', () => {
   console.log('Servidor corriendo en el puerto 3000');
